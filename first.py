42ad9ff211df20f46f0202c4d16825a4794c8ffb
tripathivaish <108897050+tripathivaish@users.noreply.github.com>
2023-09-30 16:56:44 +0530

    Add files via upload
    The upsurge in the volume of unwanted emails called spam has created an intense need for the development of more dependable and robust antispam filters. Machine learning methods of recent are being used to successfully detect and filter spam emails. We present a systematic review of some of the popular machine learning based email spam filtering approaches. Our review covers survey of the important concepts, attempts, efficiency, and the research trend in spam filtering. The preliminary discussion in the study background examines the applications of machine learning techniques to the email spam filtering process of the leading internet service providers (ISPs) like Gmail, Yahoo and Outlook emails spam filters. Discussion on general email spam filtering process, and the various efforts by different researchers in combating spam through the use machine learning techniques was done. Our review compares the strengths and drawbacks of existing machine learning approaches and the open research problems in spam filtering. We recommended deep leaning and deep adversarial learning as the future techniques that can effectively handle the menace of spam emails.

---
+++ first.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+
+df=pd.read_csv("mail_data.csv")
+print(df)
+
+
+
+data=df.where((pd.notnull(df)),'')
+
+
+data.head()#top rows of data set
+
+
+data.info()
+
+
+data.shape(5572,2)
+
+
+data.loc[data['Category']=='spam','Category',]=0
+data.loc[data['Category']=='ham','Category',]=1
+
+
+X=data['Message']
+Y=data['Category']
+
+
+print(X)
+
+print(Y)
+
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=3)
+print(X.shape)
+print(X_train)
+print(X_test.shape)
+
+
+
+print(Y.shape)
+print(Y_train)
+print(Y_test.shape)
+
+
+feature_extraction=TfidfVectorizer(min_df=1,stop_words="english",lowercase='True')
+X_train_features=features_extraction.fit_transform(X_train)
+X_test_features= feature_extraction.transform(X_test)
+
+Y_train= Y_train.astype('int')
+Y_test= Y_test.astype('int')
+
+print(X_train)
+
+print(X_train_features)
+
+
+model=LogisticRegression()
+
+model.fit(X_train_features,Y_train)
+
+
+prediction_on_training_data=model.predict(X_train_features)
+accuracy_on_training_data=accuracy_score(Y_train,prediction_on_training_data)
+
+
+print('ACCURACY ON TRAINING DATA:',accuracy_on_training_data)
+
+prediction_on_test_data= model.predict(X_test_features)
+accuracy_on_test_data=accuracy_score(Y_test,prediction_on_test_data)
+
+print('accuracy on test data:',accuracy_on_test_data)
+
+input_your_mail= [""]
+input_data_features= feature_extraction.transform(input_your_mail)
+prediction=model.predict(input_data_features)
+print(prediction)
+if prediction[0]==1:
+    print('ham email')
+else:
+    print("spam mail")
